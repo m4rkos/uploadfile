bcfffe61cf4b7eb91c499fc8b8b09feb03547921
m4rkos <marcos.j.eduardo@gmail.com>
2018-12-28 16:18:01 +0000

    Add files via upload

---
+++ ddb.php
@@ -0,0 +1,20 @@
+<?php 
+
+// DB
+    function conn(){
+        $db = 'site';
+        $host = 'localhost';
+        $usr = 'root';
+        $pass = '';
+
+        $conn = new mysqli($host, $usr, $pass, $db);
+        if ($conn == false){
+            $error = 'error on your DB connection!';
+            return $error;
+        }else{
+            return $conn;
+        }
+    }
+
+// Access to connection DB
+    $acess = conn();
